5d2c9d76ab51aacb5cb0b033045a8e4bbba4a4b0
alejandrothoughtworks <alejandro.m@thoughtworks.com>
2025-03-28 12:18:19 +0100

    fix

--- glue/variables.tf
+++ glue/variables.tf
@@ -2,8 +2,3 @@ variable "s3_bucket_name" {
   description = "S3 bucket for Glue scripts"
   default     = "my-glue-script-bucket-demo-1234563213213213213"
 }
-
-variable "glue_script_s3_path" {
-  description = "S3 path to the Glue script"
-  type        = string
-}
